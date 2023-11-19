5fa2d5287ad741ebf24b2c653df000796739c1a3
Sutej Pal <sutejpal@hotmail.com>
2023-11-19 21:59:25 +0530

    fixed pdf js error

--- src/components/pdf-viewer/pdf-viewer.component.tsx
+++ src/components/pdf-viewer/pdf-viewer.component.tsx
@@ -1,16 +1,11 @@
 import { useState, useEffect, useRef } from 'react';
 import { pdfjs, Document, Page } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
 import './style.scss';
 import ChooseTemplateIcon from './../../assets/icons/choose.png'
 
 import type { PDFDocumentProxy } from 'pdfjs-dist';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const options = {
   cMapUrl: '/cmaps/',
